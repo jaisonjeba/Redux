984dc37bde68eb15ced7b19b1a80af3912e58297
jaisonjeba <jaiprabakar5@gmail.com>
2024-08-30 23:11:34 +0530

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
-  },
+// vite.config.js
+export default {
   build: {
-    outDir: 'build', // Change this to 'build' to match Vercel's expectation
+    outDir: 'dist', // Ensure this is correct
   },
-});
+};
